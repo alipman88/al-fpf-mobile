0c3e1580a9229f307ceb781b048719151b2ef051
Matt Barry <mattwbarry@gmail.com>
2019-03-06 15:06:33 -0500

    remove unused imports from Post/index.js

--- src/core/Authenticated/Forum/components/Post/index.js
+++ src/core/Authenticated/Forum/components/Post/index.js
@@ -1,7 +1,5 @@
 import { connect } from 'react-redux'
 import { appSettings } from '@common/appSettings'
-import { currentUser } from '@common/currentUser'
-import { setupForumData } from '../../setupForumData.js'
 import { Post as PostComponent } from './Post'
 
 const mapStateToProps = state => ({
