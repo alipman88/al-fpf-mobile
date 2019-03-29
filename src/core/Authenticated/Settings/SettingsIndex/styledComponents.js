7ec364cdca80dd966ba4bb6680d311505cc1453d
arovillard <arovillard@gmail.com>
2019-03-29 16:01:09 -0400

    add missing padding bottom for logout link (#108)

--- src/core/Authenticated/Settings/SettingsIndex/styledComponents.js
+++ src/core/Authenticated/Settings/SettingsIndex/styledComponents.js
@@ -14,6 +14,7 @@ export const LogoutText = styled(Text)`
   font-size: 16;
   text-decoration: underline;
   text-align: center;
+  padding-bottom: 26;
 `
 
 export const ViewPostingsContainer = styled.View`
