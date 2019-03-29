58f3c804a26b7ed9efd607a81223489271b80909
arovillard <arovillard@gmail.com>
2019-03-29 15:43:37 -0400

    FPF08A-247 add scrollview to settings (#101)
    * add scrollview to settings
    
    * update with master and fix test warnings for profile

--- src/core/Authenticated/Settings/Account/Account.js
+++ src/core/Authenticated/Settings/Account/Account.js
@@ -5,6 +5,7 @@ import Spinner from 'react-native-loading-spinner-overlay'
 import uuid from 'uuid/v4'
 
 import { ScreenContainer } from '@components/ScreenContainer'
+import { KeyboardAwareScrollView } from '@components/KeyboardAwareScrollView'
 
 import { BackButton } from '../components/BackButton'
 import { Description } from '../components/Description'
@@ -31,68 +32,70 @@ export class Account extends React.Component {
     const { loading, navigateWithToken, user, updateUser } = this.props
     return (
       <ScreenContainer withPadding={false} grey>
-        <Spinner visible={loading || this.state.loading} />
-        <Description>
-          To edit your account, click links below to go to our website.
-        </Description>
-        <FieldLabel>Name</FieldLabel>
-        <Field>
-          <FieldText>
-            {user.first_name} {user.last_name}
-          </FieldText>
-        </Field>
-        <FieldLabel>Email</FieldLabel>
-        <Field>
-          <FieldText>{user.email}</FieldText>
-        </Field>
-        <FieldLabel>Notifications</FieldLabel>
-        <Notifications>
-          <SettingsPair>
-            <SettingsLabel>Receive Notifications</SettingsLabel>
-            <Switch
-              onValueChange={value =>
-                updateUser({ receive_push_notifications: value })
-              }
-              value={user.receive_push_notifications}
-              trackColor={{ true: '#355768' }}
-              thumbColor={Platform.select({ android: '#355768' })}
-            />
-          </SettingsPair>
-          <SettingsDescription>
-            Turn this on to be notified about each new issue of your
-            neighborhood forum.{' '}
-            <SettingsDescriptionBold>
-              This will not affect your email newsletter settings.
-            </SettingsDescriptionBold>
-          </SettingsDescription>
-        </Notifications>
+        <KeyboardAwareScrollView>
+          <Spinner visible={loading || this.state.loading} />
+          <Description>
+            To edit your account, click links below to go to our website.
+          </Description>
+          <FieldLabel>Name</FieldLabel>
+          <Field>
+            <FieldText>
+              {user.first_name} {user.last_name}
+            </FieldText>
+          </Field>
+          <FieldLabel>Email</FieldLabel>
+          <Field>
+            <FieldText>{user.email}</FieldText>
+          </Field>
+          <FieldLabel>Notifications</FieldLabel>
+          <Notifications>
+            <SettingsPair>
+              <SettingsLabel>Receive Notifications</SettingsLabel>
+              <Switch
+                onValueChange={value =>
+                  updateUser({ receive_push_notifications: value })
+                }
+                value={user.receive_push_notifications}
+                trackColor={{ true: '#355768' }}
+                thumbColor={Platform.select({ android: '#355768' })}
+              />
+            </SettingsPair>
+            <SettingsDescription>
+              Turn this on to be notified about each new issue of your
+              neighborhood forum.{' '}
+              <SettingsDescriptionBold>
+                This will not affect your email newsletter settings.
+              </SettingsDescriptionBold>
+            </SettingsDescription>
+          </Notifications>
 
-        <ExternalLink
-          hasBorder
-          onPress={() =>
-            navigateWithToken('/user', loading => this.setState({ loading }))
-          }
-        >
-          Edit account details
-        </ExternalLink>
-        <ExternalLink
-          onPress={() =>
-            // URL on site uses a generated hash, but ideally it should just be /user/subscriptions
-            // so this UUID is temporary work around
-            navigateWithToken(`/user/${uuid()}/subscriptions`, loading =>
-              this.setState({ loading })
-            )
-          }
-        >
-          Manage my email subscriptions
-        </ExternalLink>
-        <TouchableOpacity
-          onPress={() =>
-            navigateWithToken('/user', loading => this.setState({ loading }))
-          }
-        >
-          <CloseAccountText>Close my account</CloseAccountText>
-        </TouchableOpacity>
+          <ExternalLink
+            hasBorder
+            onPress={() =>
+              navigateWithToken('/user', loading => this.setState({ loading }))
+            }
+          >
+            Edit account details
+          </ExternalLink>
+          <ExternalLink
+            onPress={() =>
+              // URL on site uses a generated hash, but ideally it should just be /user/subscriptions
+              // so this UUID is temporary work around
+              navigateWithToken(`/user/${uuid()}/subscriptions`, loading =>
+                this.setState({ loading })
+              )
+            }
+          >
+            Manage my email subscriptions
+          </ExternalLink>
+          <TouchableOpacity
+            onPress={() =>
+              navigateWithToken('/user', loading => this.setState({ loading }))
+            }
+          >
+            <CloseAccountText>Close my account</CloseAccountText>
+          </TouchableOpacity>
+        </KeyboardAwareScrollView>
       </ScreenContainer>
     )
   }
--- src/core/Authenticated/Settings/Profile/Profile.js
+++ src/core/Authenticated/Settings/Profile/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Spinner from 'react-native-loading-spinner-overlay'
 import capitalize from 'lodash/capitalize'
 
+import { KeyboardAwareScrollView } from '@components/KeyboardAwareScrollView'
 import { ScreenContainer } from '@components/ScreenContainer'
 
 import { BackButton } from '../components/BackButton'
@@ -21,72 +22,74 @@ export class Profile extends React.Component {
     const { areas, navigateWithToken, profile } = this.props
     return (
       <ScreenContainer withPadding={false} grey>
-        <Spinner visible={this.state.loading} />
-        <Description>
-          Currently in app edits to your account information can’t be made here,
-          you’ll be taken to the FPF website to make these any changes by
-          clikcing the blue external links below.
-        </Description>
+        <KeyboardAwareScrollView>
+          <Spinner visible={this.state.loading} />
+          <Description>
+            Currently in app edits to your account information can’t be made
+            here, here, you’ll be taken to the FPF website to make these any
+            changes clikcing the blue external links below.
+          </Description>
 
-        <FieldLabel bottomMargin={0}>Profile type</FieldLabel>
-        <ExternalLink
-          hasBorder
-          hasLabel
-          onPress={() =>
-            navigateWithToken(`/user/profiles/${profile.id}/edit`, loading =>
-              this.setState({ loading })
-            )
-          }
-        >
-          {capitalize(profile.profile_plan.plan_type)}
-        </ExternalLink>
+          <FieldLabel bottomMargin={0}>Profile type</FieldLabel>
+          <ExternalLink
+            hasBorder
+            hasLabel
+            onPress={() =>
+              navigateWithToken(`/user/profiles/${profile.id}/edit`, loading =>
+                this.setState({ loading })
+              )
+            }
+          >
+            {capitalize(profile.profile_plan.plan_type)}
+          </ExternalLink>
 
-        <FieldLabel>Address</FieldLabel>
-        <Field>
-          <FieldText>
-            {profile.street_number} {profile.street_name}{' '}
-            {Boolean(profile.apt_number) && `# ${profile.apt_number}`}
-          </FieldText>
-          <FieldText>{profile.city}</FieldText>
-          <FieldText>{profile.state}</FieldText>
-        </Field>
-        <FieldLabel>Forum(s)</FieldLabel>
-        <Field>
-          {profile.area_ids.map(areaId => (
-            <FieldText key={areaId}>{areas[areaId].name}</FieldText>
-          ))}
-        </Field>
-        <ExternalLink
-          hasBorder
-          onPress={() =>
-            navigateWithToken(`/user/profiles/${profile.id}/edit`, loading =>
-              this.setState({ loading })
-            )
-          }
-        >
-          Change your address
-        </ExternalLink>
-        <ExternalLink
-          hasBorder
-          onPress={() =>
-            navigateWithToken(
-              '/user/profiles/new?disable_plan_type_change=true&profile%5Bprofile_plan_id%5D=3',
-              loading => this.setState({ loading })
-            )
-          }
-        >
-          Add a second home
-        </ExternalLink>
-        <ExternalLink
-          onPress={() =>
-            navigateWithToken(
-              '/user/profiles/new?profile%5Bprofile_plan_id%5D=4',
-              loading => this.setState({ loading })
-            )
-          }
-        >
-          Add government profile
-        </ExternalLink>
+          <FieldLabel>Address</FieldLabel>
+          <Field>
+            <FieldText>
+              {profile.street_number} {profile.street_name}{' '}
+              {Boolean(profile.apt_number) && `# ${profile.apt_number}`}
+            </FieldText>
+            <FieldText>{profile.city}</FieldText>
+            <FieldText>{profile.state}</FieldText>
+          </Field>
+          <FieldLabel>Forum(s)</FieldLabel>
+          <Field>
+            {profile.area_ids.map(areaId => (
+              <FieldText key={areaId}>{areas[areaId].name}</FieldText>
+            ))}
+          </Field>
+          <ExternalLink
+            hasBorder
+            onPress={() =>
+              navigateWithToken(`/user/profiles/${profile.id}/edit`, loading =>
+                this.setState({ loading })
+              )
+            }
+          >
+            Change your address
+          </ExternalLink>
+          <ExternalLink
+            hasBorder
+            onPress={() =>
+              navigateWithToken(
+                '/user/profiles/new?disable_plan_type_change=true&profile%5Bprofile_plan_id%5D=3',
+                loading => this.setState({ loading })
+              )
+            }
+          >
+            Add a second home
+          </ExternalLink>
+          <ExternalLink
+            onPress={() =>
+              navigateWithToken(
+                '/user/profiles/new?profile%5Bprofile_plan_id%5D=4',
+                loading => this.setState({ loading })
+              )
+            }
+          >
+            Add government profile
+          </ExternalLink>
+        </KeyboardAwareScrollView>
       </ScreenContainer>
     )
   }
