ad34016a4f745787b4ee784d0afce15a06d09a10
Aaron McLeod <aaron.g.mcleod@gmail.com>
2019-04-03 13:40:33 -0400

    [FPF08A-241] Prevent opening drawer menu when logged out (#126)
    * hook up drawer to login state
    
    * add spinner to container component

---
+++ src/App/Container.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import SideMenu from 'react-native-side-menu'
+
+import { currentUser } from '@common/currentUser'
+import { DrawerContext } from './context'
+import { DrawerMenu } from '@components/DrawerMenu'
+import { SwitchNavigatorContainer } from '@core/switchNavigator'
+import { Spinner } from './Spinner'
+import { getDimensions, isTabletWidth } from '@common/utils/size'
+import navigationService from '@common/utils/navigationService'
+
+class ContainerComponent extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // defined here so we can put it in inital state
+    this.setDrawerOpenState = open => {
+      this.setState(() => ({
+        open
+      }))
+    }
+
+    this.state = {
+      open: false,
+      setDrawerOpenState: this.setDrawerOpenState
+    }
+  }
+
+  render() {
+    const { accessToken } = this.props
+    return (
+      <DrawerContext.Provider value={this.state}>
+        <SideMenu
+          menu={<DrawerMenu />}
+          isOpen={this.state.open}
+          onChange={open => this.setDrawerOpenState(open)}
+          openMenuOffset={
+            isTabletWidth()
+              ? (getDimensions().width * 1) / 3
+              : (getDimensions().width * 2) / 3
+          }
+          bounceBackOnOverdraw={false}
+          disableGestures={!accessToken}
+        >
+          <Spinner />
+          <SwitchNavigatorContainer
+            ref={navigatorRef => {
+              navigationService.setTopLevelNavigator(navigatorRef)
+            }}
+          />
+        </SideMenu>
+      </DrawerContext.Provider>
+    )
+  }
+}
+
+ContainerComponent.propTypes = {
+  accessToken: PropTypes.string
+}
+
+const mapStateToProps = state => ({
+  accessToken: currentUser.selectors.getAccessToken(state)
+})
+
+export const Container = connect(mapStateToProps)(ContainerComponent)
--- src/App/index.js
+++ src/App/index.js
@@ -1,37 +1,16 @@
 import React from 'react'
-import SideMenu from 'react-native-side-menu'
 import { Linking } from 'react-native'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
-import { DrawerContext } from './context'
 import { store, persistor } from '@common/store'
 import { currentUser } from '@common/currentUser'
-import { DrawerMenu } from '@components/DrawerMenu'
-import { SwitchNavigatorContainer } from '@core/switchNavigator'
 import { AppError } from '@components/AppError'
-import { getDimensions, isTabletWidth } from '@common/utils/size'
 import { parseDeepLink } from '@common/utils/parseDeepLink'
-import { Spinner } from './Spinner'
 import navigationService from '@common/utils/navigationService'
+import { Container } from './Container'
 
 export class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    // defined here so we can put it in inital state
-    this.setDrawerOpenState = open => {
-      this.setState(() => ({
-        open
-      }))
-    }
-
-    this.state = {
-      open: false,
-      setDrawerOpenState: this.setDrawerOpenState
-    }
-  }
-
   componentDidMount() {
     Linking.addEventListener('url', this.handleOpenURL)
   }
@@ -52,26 +31,7 @@ export class App extends React.Component {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <React.Fragment>
-            <DrawerContext.Provider value={this.state}>
-              <SideMenu
-                menu={<DrawerMenu />}
-                isOpen={this.state.open}
-                onChange={open => this.setDrawerOpenState(open)}
-                openMenuOffset={
-                  isTabletWidth()
-                    ? (getDimensions().width * 1) / 3
-                    : (getDimensions().width * 2) / 3
-                }
-                bounceBackOnOverdraw={false}
-              >
-                <Spinner />
-                <SwitchNavigatorContainer
-                  ref={navigatorRef => {
-                    navigationService.setTopLevelNavigator(navigatorRef)
-                  }}
-                />
-              </SideMenu>
-            </DrawerContext.Provider>
+            <Container />
             <AppError />
           </React.Fragment>
         </PersistGate>
