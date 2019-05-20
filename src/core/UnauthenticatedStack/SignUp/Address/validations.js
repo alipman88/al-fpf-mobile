3d168a695d44a5d6e4a847f3d7f4a78240f9b5b5
Matt Barry <mattwbarry@gmail.com>
2019-05-20 16:30:20 -0400

    [#165400218] trim whitespace from streetName and city during validation in signup

--- src/core/UnauthenticatedStack/SignUp/Address/validations.js
+++ src/core/UnauthenticatedStack/SignUp/Address/validations.js
@@ -5,8 +5,14 @@ export const validations = yup.object().shape({
     .string()
     .matches(/^[0-9]*$/, 'Please enter a valid House/building number')
     .required('Street number is a required field'),
-  streetName: yup.string().required('Street name is a required field'),
+  streetName: yup
+    .string()
+    .trim()
+    .required('Street name is a required field'),
   apt: yup.string(),
-  city: yup.string().required(),
+  city: yup
+    .string()
+    .trim()
+    .required(),
   state: yup.string().required()
 })
