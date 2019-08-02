09acb062fd8a96ee980f86fc17a4e4b1d181b252
Noah Harrison <noah@frontporchforum.com>
2019-08-02 12:05:12 -0400

    [#167580373] added IAP config and react-native-iap to project files
    Note that Android configuration is not complete as we are not currently
    implementing support for Google Play purchases.

--- android/app/src/main/java/com/frontporchforum/MainApplication.java
+++ android/app/src/main/java/com/frontporchforum/MainApplication.java
@@ -3,6 +3,7 @@ package com.frontporchforum;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dooboolab.RNIap.RNIapPackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new RNDeviceInfo(),
           new MainReactPackage(),
+            new RNIapPackage(),
             new NetInfoPackage(),
           new SplashScreenReactPackage(),
           new MapsPackage(),
