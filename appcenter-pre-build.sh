a887878b84ef12083e5943327b4825b509713838
Noah Harrison <noah@frontporchforum.com>
2019-09-09 11:08:56 -0400

    [#168347035] added ENVIRONMENT and ROLLBAR_API_KEY env vars to appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,5 +2,7 @@
 
 echo "API_HOST=$API_HOST" > .env
 echo "API_KEY=$API_KEY" >> .env
+echo "ENVIRONMENT=$ENVIRONMENT" >> .env
 echo "GOOGLE_MAPS_API_KEY=$GOOGLE_MAPS_API_KEY" >> .env
+echo "ROLLBAR_API_KEY=$ROLLBAR_API_KEY" >> .env
 echo "WEBSITE_HOST=$WEBSITE_HOST" >> .env
