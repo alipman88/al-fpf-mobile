809eb38c2e44a140a418c07966f0737831d71c25
Matt Barry <mattwbarry@gmail.com>
2019-11-06 15:14:52 -0500

    [#169264403] use custom regex for frontend email validation instead of yup.email()

--- src/core/UnauthenticatedStack/Login/validations.js
+++ src/core/UnauthenticatedStack/Login/validations.js
@@ -1,9 +1,14 @@
 import * as yup from 'yup'
 
+export const validateEmail = string => {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(string.toLowerCase())
+}
+
 export const validations = yup.object().shape({
   email: yup
     .string()
     .required()
-    .email(),
+    .test('email', 'email must be a valid email', validateEmail),
   password: yup.string().required()
 })
