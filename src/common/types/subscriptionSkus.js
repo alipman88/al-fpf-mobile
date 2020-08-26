edd6a5934c9051823410d005d27ac2701a3969b0
mattwbarry <mattwbarry@gmail.com>
2020-08-26 11:08:35 -0400

    [#174317358] add enhanced business tier to apple payment options

--- src/common/types/subscriptionSkus.js
+++ src/common/types/subscriptionSkus.js
@@ -2,5 +2,10 @@ import { Platform } from 'react-native'
 
 // Hard-coded list of IAP SKUs, by platform
 export const subscriptionSkus = Platform.select({
-  ios: ['standard_business_monthly', 'standard_business_yearly']
+  ios: [
+    'standard_business_monthly',
+    'standard_business_yearly',
+    'enhanced_business_monthly',
+    'enhanced_business_yearly'
+  ]
 })
