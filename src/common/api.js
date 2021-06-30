75346f643e7ba5f75a6c4c3c00a3a3af99ad866b
Aaron Lipman <alipman88@gmail.com>
2021-06-29 23:17:40 -0400

    [#177966320] improve axios error stack traces
    The stack traces of errors resulting from failed asynchronous axios
    requests do not include the actual code that called an axios request in
    the first place. This makes debugging very difficult - use an axios
    interceptor to append the original stack trace so that the code which
    triggered the failing axios request may be determined via rollbar.

--- src/common/api.js
+++ src/common/api.js
@@ -3,12 +3,35 @@ import axios from 'axios'
 import { Config } from '@common/config'
 import { currentUser } from './currentUser'
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: Config.API_HOST,
   headers: {
     'X-API-KEY': `Bearer ${Config.API_KEY}`,
   },
 })
+
+// The stack traces of errors resulting from failed asynchronous axios
+// requests do not include the actual code that called an axios request in
+// the first place. This makes debugging very difficult - use an axios
+// interceptor to append the original stack trace so that the code which
+// triggered the failing axios request may be determined via rollbar.
+// https://github.com/axios/axios/issues/2387#issuecomment-652242713
+api.interceptors.request.use((config) => {
+  config.errorContext = new Error('Thrown at:')
+  return config
+})
+
+api.interceptors.response.use(undefined, async (error) => {
+  const originalStackTrace = error.config?.errorContext?.stack
+
+  if (originalStackTrace) {
+    error.stack = `${error.stack}\n${originalStackTrace}`
+  }
+
+  throw error
+})
+
+export { api }
 
 const getAuthorizedHeaders = (state) => ({
   Authorization: `Bearer ${currentUser.selectors.getAccessToken(state)}`,
