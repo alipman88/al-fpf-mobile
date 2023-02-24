c415060aabfdf63103124ce95e544edb9cf1b550
Noah Harrison <noah@frontporchforum.com>
2023-02-24 16:52:08 -0500

    [#183333016] show post disclaimers
    Updated post truncation logic and display so that disclaimers are taken into
    account when determining whether to truncate the post, and are not shown if
    the post is truncated.
    
    The plain text version of a disclaimer is shown because it would be tricky
    to render the HTML version.

--- src/common/utils/truncateText.js
+++
@@ -1,7 +0,0 @@
-import truncate from 'lodash/truncate'
-
-export const truncateText = (content, postTruncateLength, shouldTruncate) => {
-  return shouldTruncate && content.length > postTruncateLength
-    ? truncate(content, { length: postTruncateLength, separator: /\s/ })
-    : content
-}
