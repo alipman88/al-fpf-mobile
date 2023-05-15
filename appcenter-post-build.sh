410bd344378e01594da64ac286370572fbe2db8e
Noah Harrison <noah@frontporchforum.com>
2023-05-15 12:07:42 -0400

    Better fixed Rollbar file reference in `appcenter-post-build.sh`

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -2,11 +2,11 @@
 
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
   if [ "$BUILD_PLATFORM" == "ios" ]; then
-    yarn run react-native bundle --platform ios --entry-file index.ios.js --dev false --bundle-output \
+    yarn run react-native bundle --platform ios --entry-file index.js --dev false --bundle-output \
     ios/main.jsbundle --assets-dest ios --sourcemap-output sourcemap.ios.js --sourcemap-sources-root ./
     MINIFIED_URL='http://reactnativehost/main.jsbundle'
   elif [ "$BUILD_PLATFORM" == "android" ]; then
-    yarn run react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output \
+    yarn run react-native bundle --platform android --dev false --entry-file index.js --bundle-output \
     android/index.android.bundle --assets-dest android/app/src/main/res/ --sourcemap-output \
     sourcemap.android.js --sourcemap-sources-root ./
     MINIFIED_URL='http://reactnativehost/index.android.bundle'
@@ -20,5 +20,5 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     -F version="$APPCENTER_BUILD_ID.$BUILD_PLATFORM" \
     -F minified_url=$MINIFIED_URL \
     -F source_map="@sourcemap.$BUILD_PLATFORM.js" \
-    -F index.$BUILD_PLATFORM.js="@index.$BUILD_PLATFORM.js"
+    -F index.js="@index.js"
 fi
