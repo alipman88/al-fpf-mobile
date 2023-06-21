e5f844a898c62ff4b5f14f22e4d748d69b4d69a5
Noah Harrison <noah@frontporchforum.com>
2023-06-21 16:43:37 -0400

    [#185386223] fixed scrolling in the "Account & Help" view
    A small phone or a large number of profiles would cause information at the
    bottom of this view to be unreachable without a scroll view.

--- src/core/Authenticated/Settings/SettingsIndex/SettingsIndex.js
+++ src/core/Authenticated/Settings/SettingsIndex/SettingsIndex.js
@@ -7,6 +7,7 @@ import Spinner from 'react-native-loading-spinner-overlay'
 
 import { Config } from '@common/config'
 import { ScreenContainer } from '@components/ScreenContainer'
+import { KeyboardAwareScrollView } from '@components/KeyboardAwareScrollView'
 import { getProfileDisplayName } from '@common/utils/getProfileDisplayName'
 import { NavLink } from '../components/NavLink'
 import { FieldLabel } from '../components/FieldLabel'
@@ -54,83 +55,87 @@ export class SettingsIndex extends React.Component {
     const { navigateWithToken, navigation, user } = this.props
     return (
       <ScreenContainer grey withPadding={false}>
-        <Spinner visible={this.state.loading} />
-        <Container>
-          <Navigation>
-            <FieldHeading>Account</FieldHeading>
-            <FieldLabel style={{ marginTop: 12 }}>My account</FieldLabel>
-            <NavLink
-              linkText={`${user.first_name} ${user.last_name}`}
-              onPress={() => navigation.navigate('Account')}
-              hasBorder={true}
-            />
-            <FieldLabel>My profile(s)</FieldLabel>
-            {user.profiles.map((profile, i, arr) => (
+        <KeyboardAwareScrollView>
+          <Spinner visible={this.state.loading} />
+          <Container>
+            <Navigation>
+              <FieldHeading>Account</FieldHeading>
+              <FieldLabel style={{ marginTop: 12 }}>My account</FieldLabel>
               <NavLink
-                linkText={getProfileDisplayName(profile)}
-                key={profile.id}
-                onPress={() =>
-                  navigation.navigate('Profile', { profileId: profile.id })
-                }
-                hasBorder={arr.length === i + 1}
+                linkText={`${user.first_name} ${user.last_name}`}
+                onPress={() => navigation.navigate('Account')}
+                hasBorder={true}
               />
-            ))}
-            <TouchableOpacity
-              onPress={() =>
-                navigateWithToken('/user/posts', (loading) =>
-                  this.setState({ loading })
-                )
-              }
-            >
-              <ExternalLinkContainer style={{ marginTop: 18 }}>
-                <ViewPostings>View my postings</ViewPostings>
-                <Image source={linkIcon} />
-              </ExternalLinkContainer>
+              <FieldLabel>My profile(s)</FieldLabel>
+              {user.profiles.map((profile, i, arr) => (
+                <NavLink
+                  linkText={getProfileDisplayName(profile)}
+                  key={profile.id}
+                  onPress={() =>
+                    navigation.navigate('Profile', { profileId: profile.id })
+                  }
+                  hasBorder={arr.length === i + 1}
+                />
+              ))}
+              <TouchableOpacity
+                onPress={() =>
+                  navigateWithToken('/user/posts', (loading) =>
+                    this.setState({ loading })
+                  )
+                }
+              >
+                <ExternalLinkContainer style={{ marginTop: 18 }}>
+                  <ViewPostings>View my postings</ViewPostings>
+                  <Image source={linkIcon} />
+                </ExternalLinkContainer>
+              </TouchableOpacity>
+              <FieldHeading style={{ marginTop: 30 }}>Help</FieldHeading>
+              <TouchableOpacity
+                onPress={() => openFpfUrl('https://help.frontporchforum.com')}
+              >
+                <ExternalLinkContainer>
+                  <ViewPostings>Member Support</ViewPostings>
+                  <Image source={linkIcon} />
+                </ExternalLinkContainer>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() =>
+                  openFpfUrl(`${Config.WEBSITE_HOST}/privacy-policy`)
+                }
+              >
+                <ExternalLinkContainer>
+                  <ViewPostings>Privacy Policy</ViewPostings>
+                  <Image source={linkIcon} />
+                </ExternalLinkContainer>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() =>
+                  openFpfUrl(`${Config.WEBSITE_HOST}/terms-of-use`)
+                }
+              >
+                <ExternalLinkContainer>
+                  <ViewPostings>Terms of Use</ViewPostings>
+                  <Image source={linkIcon} />
+                </ExternalLinkContainer>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => openFpfUrl(`${Config.WEBSITE_HOST}/about-us`)}
+              >
+                <ExternalLinkContainer>
+                  <ViewPostings>About FPF</ViewPostings>
+                  <Image source={linkIcon} />
+                </ExternalLinkContainer>
+              </TouchableOpacity>
+              <Version>
+                v{DeviceInfo.getVersion()} #{DeviceInfo.getBuildNumber()}
+                {envDisplay}
+              </Version>
+            </Navigation>
+            <TouchableOpacity onPress={() => this.logoutUser()}>
+              <LogoutText>Log out</LogoutText>
             </TouchableOpacity>
-            <FieldHeading style={{ marginTop: 30 }}>Help</FieldHeading>
-            <TouchableOpacity
-              onPress={() => openFpfUrl('https://help.frontporchforum.com')}
-            >
-              <ExternalLinkContainer>
-                <ViewPostings>Member Support</ViewPostings>
-                <Image source={linkIcon} />
-              </ExternalLinkContainer>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() =>
-                openFpfUrl(`${Config.WEBSITE_HOST}/privacy-policy`)
-              }
-            >
-              <ExternalLinkContainer>
-                <ViewPostings>Privacy Policy</ViewPostings>
-                <Image source={linkIcon} />
-              </ExternalLinkContainer>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => openFpfUrl(`${Config.WEBSITE_HOST}/terms-of-use`)}
-            >
-              <ExternalLinkContainer>
-                <ViewPostings>Terms of Use</ViewPostings>
-                <Image source={linkIcon} />
-              </ExternalLinkContainer>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => openFpfUrl(`${Config.WEBSITE_HOST}/about-us`)}
-            >
-              <ExternalLinkContainer>
-                <ViewPostings>About FPF</ViewPostings>
-                <Image source={linkIcon} />
-              </ExternalLinkContainer>
-            </TouchableOpacity>
-            <Version>
-              v{DeviceInfo.getVersion()} #{DeviceInfo.getBuildNumber()}
-              {envDisplay}
-            </Version>
-          </Navigation>
-          <TouchableOpacity onPress={() => this.logoutUser()}>
-            <LogoutText>Log out</LogoutText>
-          </TouchableOpacity>
-        </Container>
+          </Container>
+        </KeyboardAwareScrollView>
       </ScreenContainer>
     )
   }
