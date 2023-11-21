e947a3595eb1116e15d57adfc2b72865d55f8e04
Noah Harrison <noah@frontporchforum.com>
2023-11-21 16:55:44 -0500

    [#185883871] fixed applying (and testing) max height in `SizedImage`
    In the `SizedImage` component that resizes an image loaded from a URL using some
    configured max width and/or height, the algorithm failed to apply a max height
    when no max width was set (because `maxWidth` is undefined when the component
    is first loaded).  Fixed this.
    
    Also fixed the test, which wasn't asserting the width and height styles correctly,
    and expanded the test cases.

--- src/common/components/SizedImage/__tests__/index.test.js
+++ src/common/components/SizedImage/__tests__/index.test.js
@@ -6,32 +6,95 @@ import { Image } from 'react-native'
 import { SizedImage } from '../index'
 
 describe('SizedImage', () => {
-  test('renders image at width and height when uri is present', () => {
-    const image_url = 'someimage somewhere'
-    const props = { uri: image_url, maxHeight: 50 }
+  /* eslint-disable prettier/prettier */
+  const sizes = [
+    // No max values set
+    {
+      name: 'No max sizes',
+      width: 200, height: 100, expectedWidth: 200, expectedHeight: 100
+    },
 
-    // mock out the getSize call
-    const getSizeMock = jest.spyOn(Image, 'getSize')
-    getSizeMock.mockImplementation(() => {})
+    // Max width set
+    {
+      name: 'Landscape larger than max width',
+      width: 200, height: 100, maxWidth: 50, expectedWidth: 50, expectedHeight: 25
+    },
+    {
+      name: 'Portrait larger than max width',
+      width: 100, height: 200, maxWidth: 50, expectedWidth: 50, expectedHeight: 100
+    },
 
-    render(<SizedImage {...props} />)
+    // Max height set
+    {
+      name: 'Landscape larger than max height',
+      width: 200, height: 100, maxHeight: 50, expectedWidth: 100, expectedHeight: 50
+    },
+    {
+      name: 'Portrait larger than max height',
+      width: 100, height: 200, maxHeight: 50, expectedWidth: 25, expectedHeight: 50
+    },
 
-    expect(getSizeMock).toHaveBeenCalledTimes(1)
+    // Max width and height set
+    {
+      name: 'Smaller than max sizes',
+      width: 200, height: 100, maxWidth: 300, maxHeight: 300, expectedWidth: 200, expectedHeight: 100
+    },
+    {
+      name: 'Landscape larger than max width and but not height',
+      width: 200, height: 100, maxWidth: 50, maxHeight: 500, expectedWidth: 50, expectedHeight: 25
+    },
+    {
+      name: 'Portrait larger than max width and but not height',
+      width: 100, height: 200, maxWidth: 50, maxHeight: 500, expectedWidth: 50, expectedHeight: 100
+    },
+    {
+      name: 'Landscape larger than max height and but not width',
+      width: 200, height: 100, maxWidth: 500, maxHeight: 50, expectedWidth: 100, expectedHeight: 50
+    },
+    {
+      name: 'Portrait larger than max height and but not width',
+      width: 100, height: 200, maxWidth: 500, maxHeight: 50, expectedWidth: 25, expectedHeight: 50
+    },
+    {
+      name: 'Landscape larger than max width and height',
+      width: 200, height: 100, maxWidth: 50, maxHeight: 50, expectedWidth: 50, expectedHeight: 25
+    },
+    {
+      name: 'Portrait larger than max width and height',
+      width: 100, height: 200, maxWidth: 50, maxHeight: 50, expectedWidth: 25, expectedHeight: 50
+    },
+  ]
+  /* eslint-enable prettier/prettier */
 
-    const args = getSizeMock.mock.calls[0]
-    expect(args[0]).toEqual(image_url)
+  sizes.forEach((size) => {
+    test(`renders image at width and height when uri is present: ${size.name}`, () => {
+      const imageUrl = 'someimage somewhere'
+      const props = {
+        uri: imageUrl,
+        maxHeight: size.maxHeight,
+        maxWidth: size.maxWidth,
+      }
 
-    // second argument is the success callback that we need to invoke
-    const success_callback = args[1]
-    TestRenderer.act(() => {
-      success_callback(200, 100)
+      // mock out the getSize call
+      const getSizeMock = jest.spyOn(Image, 'getSize')
+      getSizeMock.mockImplementation(() => {})
+
+      render(<SizedImage {...props} />)
+
+      const args = getSizeMock.mock.calls[0]
+      expect(args[0]).toEqual(imageUrl)
+
+      // second argument is the success callback that we need to invoke
+      const successCallback = args[1]
+      TestRenderer.act(() => {
+        successCallback(size.width, size.height)
+      })
+      getSizeMock.mockRestore()
+
+      const image = screen.getByTestId('image')
+      expect(image).toHaveStyle({ width: size.expectedWidth })
+      expect(image).toHaveStyle({ height: size.expectedHeight })
     })
-
-    const image = screen.getByTestId('image')
-    expect(image).toHaveStyle('width', 100)
-    expect(image).toHaveStyle('height', 50)
-
-    getSizeMock.mockRestore()
   })
 
   test('does not render image or set width and height when uri is missing', () => {
