26496631edd8004fd465726ff9a9b78b92941f2c
Noah Harrison <noah@frontporchforum.com>
2025-01-09 14:22:17 -0500

    [TECH-6504] fixed `Grass` remaining visible when drawer is closed
    The grass border at the bottom of the drawer menu was remaining visible and
    covering up the main interface even when the drawer was closed. Removed the
    z-index, which doesn't seem to serve any necessar

--- src/common/components/Grass/styledComponents.js
+++ src/common/components/Grass/styledComponents.js
@@ -5,7 +5,6 @@ export const GrassContainer = styled.View`
   background-color: #e3eddd;
   height: ${({ height }) => height || screenSize({ sm: 90 }, 140)}px;
   width: 100%;
-  z-index: 100;
 `
 
 export const GrassBorder = styled.Image`
