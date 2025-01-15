786f6fede59bf264b2852a5694f1ccf672ed1c01
Noah Harrison <noah@frontporchforum.com>
2025-01-15 11:18:42 -0500

    [TECH-6504] use yarn 4 in appcenter

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# Use yarn 4 instead of yarn 1
+#
+# https://github.com/microsoft/appcenter/issues/2134#issuecomment-1942319970
+#
+# Use corepack to install yarn 4
+corepack enable
+#
+# Install dependencies here instead of the appcenter step
+echo "Install dependencies"
+yarn install --immutable
+#
+# Overwrite yarn.js to stop appcenter from calling it with wrong parameters
+yarnVersion=$(corepack yarn --version)
+echo "" > /Users/runner/.cache/node/corepack/yarn/$yarnVersion/yarn.js
+
 # Install the version of CocoaPods listed in ios/Podfile.lock
 # https://github.com/microsoft/appcenter/issues/95
 # https://github.com/CocoaPods/CocoaPods/issues/12226
